1c15e7a5ede375aeb8a4dbf18a7b510ba9779b29
Real_PainNonsense <98066737+RealPainNonsense@users.noreply.github.com>
2022-04-30 13:48:31 +0800

    Update Work_at_a_Pizza_Place.lua

--- Work_at_a_Pizza_Place.lua
+++ Work_at_a_Pizza_Place.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Work at a Pizza Place", "DarkTheme")
+local Window = Library.CreateLib("Work at a Pizza Place", "GrapeTheme")
 
 -- LocalPlayer
 
